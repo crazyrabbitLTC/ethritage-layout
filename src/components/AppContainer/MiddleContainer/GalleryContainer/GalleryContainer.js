141eb9608636e64c91b919c320d0249c7552def9
Dennison Bertram <dennison@dennisonbertram.com>
2019-05-30 18:14:18 -0400

    Minor Cleanup + Component Containers

--- src/components/AppContainer/MiddleContainer/GalleryContainer/GalleryContainer.js
+++ src/components/AppContainer/MiddleContainer/GalleryContainer/GalleryContainer.js
@@ -1,17 +1,7 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import ThumbnailContainer from "./ThumbnailContainer/ThumbnailContainer";
-import ImageContainer from "./ImageContainer/ImageContainer";
 
 function GalleryContainer() {
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="galleryContainer">
-THIS IS GALLERY CONTAINER
-    </div>
-  );
+  return <div className="galleryContainer">THIS IS GALLERY CONTAINER.</div>;
 }
 
 export default GalleryContainer;
